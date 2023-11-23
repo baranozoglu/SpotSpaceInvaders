2935b6c7ecda8311250c15ca7898db533433a5da
baranozoglu <abaranozoglu@gmail.com>
2023-11-23 14:57:22 +0100

    fixed path

--- src/main/java/properties/TextProperties.java
+++ src/main/java/properties/TextProperties.java
@@ -2,6 +2,6 @@ package properties;
 
 public class TextProperties {
     public static final String SEPARATOR = "~~~~";
-    public static final String RADARS_TXT = "resource/radars.txt";
-    public static final String SPACE_INVADERS_TXT = "resource/spaceInvaders.txt";
+    public static final String RADARS_TXT = "./src/main/resource/radars.txt";
+    public static final String SPACE_INVADERS_TXT = "./src/main/resource/spaceInvaders.txt";
 }
